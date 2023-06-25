399658f4379ab042d7952df111a5ff8bac1b635f
Kyyraidn <137687108+Kyyraidn@users.noreply.github.com>
2023-06-25 18:37:42 +0700

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>experiment</title>
+</head>
+
+<style>
+  .cuy span {
+    height: 100px;
+    width: 100px;
+  }
+</style>
+
+<body>
+  <?php
+  $id = $_GET["id"];
+  ?>
+  
+  <form method="get">
+    <input type="number" name="id" value="<?php echo $id ?>">
+    <button type="submit">kirim</button>
+    </form>
+    <div>
+      <a href="https://unknownstore.bukaolshop.site/produk/5-diamond-mobile-legend-1876983/?catatan=<?php echo $id ?>">10 dimaond mlbb</a>
+    </div>
+</body>
+</html>
